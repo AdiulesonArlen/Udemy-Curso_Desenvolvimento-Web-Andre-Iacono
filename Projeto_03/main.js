a9f55050448191f714494611c33ecf2a0c8bd14e
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2024-09-03 23:05:52 -0300

    Projeto 03

---
+++ Projeto_03/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Digite seu primeiro nome: ");
+let lastName = prompt("Digite seu último nome: ");
+let age = parseInt(prompt("Digite sua idade: "));
+
+const age2 = 2;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age + age2);
