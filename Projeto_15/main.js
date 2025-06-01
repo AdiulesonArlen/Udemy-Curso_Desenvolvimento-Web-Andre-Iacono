fce596e79a1a7cbc782f7a8c5f6370c220ebd445
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2025-06-01 17:53:47 -0300

    projeto solto hora formatada

---
+++ Projeto_15/main.js
@@ -0,0 +1,7 @@
+let numberList = document.getElementById('numberList')
+
+for (i = 1; i <= 5; i++) {
+  let listItem = document.createElement('li');
+  listItem.textContent = "Item " + i;
+  numberList.appendChild(listItem);
+}
